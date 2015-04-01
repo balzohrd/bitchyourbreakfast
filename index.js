ef4d296d418b23e81d262444444f2fbdc9bbc368
Gal Azencot <gal_azencot@ymail.com>
2015-04-01 13:39:31 -0700

    Wait a minute. I might just have an idea. They'll be looking for us at Maury's right? But they won't be looking for... not us.

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var express=require('express'),
 app=express(),
 path=require('path');
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-})
+app.use(express.static(__dirname + '/public')); 
+
+//app.get('/', function(req, res){
+//  res.sendFile(path.join(__dirname + '/index.html'));
+//})
 
 app.listen(process.env.PORT || 3000 )
