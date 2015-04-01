5221864576098ac7aa30763c92ca709c7734b5be
Gal Azencot <gal_azencot@ymail.com>
2015-03-31 22:12:40 -0700

    Moisture is the essence of wetness, and wetness is the essence of beauty.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app=express(),
 path=require('path');
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/public'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(process.env.PORT || 3000 )
